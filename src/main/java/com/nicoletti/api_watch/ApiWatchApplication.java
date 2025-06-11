23e933637873f4b7111b8f5b2c8e55030346f85c
André Nicoletti <andregnicoletti@gmail.com>
2025-06-11 12:05:49 -0300

    first commit

---
+++ src/main/java/com/nicoletti/api_watch/ApiWatchApplication.java
@@ -0,0 +1,13 @@
+package com.nicoletti.api_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWatchApplication.class, args);
+	}
+
+}
